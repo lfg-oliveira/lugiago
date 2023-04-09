130759a654347d373eab94778159fccb6de8b8f2
Luiz Felipe Guidorizzi de Oliveira <lfg.oliveira@protonmail.com>
2023-04-09 03:28:44 -0300

    Criando model (WIP)

--- src/main/java/com/lugiago/Controller/Controller.java
+++ src/main/java/com/lugiago/Controller/Controller.java
@@ -1,7 +1,32 @@
 package com.lugiago.Controller;
 
-import java.lang.reflect.InvocationTargetException;
+import com.lugiago.db.Database;
+
+import java.sql.*;
 
 public abstract class Controller {
+    protected Controller() throws SQLException, ClassNotFoundException {
+    }
+
+    /**
+     * Pega informações do banco de dados
+     * @param sql
+     * @return
+     */
+    public abstract ResultSet getData(String sql);
+
+    /**
+     * Atualiza informações no banco de dados
+     * @param sql
+     * @param newData
+     */
+    public abstract void updateData(String sql, String[] newData);
+
+    /**
+     * Insere novas informações no banco
+     * @param sql
+     */
+    public abstract void insertData(String sql);
+
 
 }
--- src/main/java/com/lugiago/View/Menu.java
+++ src/main/java/com/lugiago/View/Menu.java
@@ -7,6 +7,9 @@ public class Menu {
     String state = "main";
     public Menu() {
         menuMap.put("main", "Menu Principal:\n"+
-                "\n1 - Cadastrar");
+                "\n1 - Cadastrar Funcionário"+
+                "\n2 - Planejamento de Turnos"+
+                "\n3 - Trocar plantões"+
+                "\n4 - Imprimir turnos por dia do mês de Abril");
     }
 }
