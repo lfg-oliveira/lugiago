7897b556e4414ecac3b18e1328287b19d3913340
Tiago <tiagoxp.desenvolvedor@gmail.com>
2023-04-10 08:42:18 -0300

    Adicionado Script de Criação do DB

---
+++ db-script.sql
@@ -0,0 +1,36 @@
+drop database if exists hospital;
+create database hospital;
+use hospital;
+
+drop table if exists Turno;
+drop table if exists Funcionario;
+drop table if exists Cargo;
+
+drop table if exists Cargo;
+create table Cargo(
+Id bigint not null primary key auto_increment,
+TipoCodigo varchar(10) not null,
+Descricao varchar(50) not null
+);
+
+drop table if exists Funcionario;
+create table Funcionario(
+Id bigint not null primary key auto_increment,
+Codigo bigint not null unique key,
+Nome varchar(80) not null,
+idCargo bigint not null,
+foreign key (idCargo) references Cargo(Id)
+);
+
+drop table if exists Turno;
+create table Turno (
+Id bigint not null primary key auto_increment,
+IdFuncionario bigint not null,
+Data Datetime not null,
+foreign key (IdFuncionario) references Funcionario(Id)
+);
+
+insert into Cargo(TipoCodigo, Descricao)
+values ('Enfermeiro', 'COREN')
+	  ,('Médico', 'CRM')
+	  ,('Residente', 'CRM');
