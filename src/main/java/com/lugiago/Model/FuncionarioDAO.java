8e56332bbd90f5c1ac873c4e40ab9a5c390981d6
android <android@LABFAE.local>
2023-04-10 22:31:42 -0300

    Ajustado GetAllFuncionarios

--- src/main/java/com/lugiago/Model/FuncionarioDAO.java
+++ src/main/java/com/lugiago/Model/FuncionarioDAO.java
@@ -17,67 +17,20 @@ import java.util.logging.Logger;
  */
 public class FuncionarioDAO {
 
-    private List<Funcionario> funcionarios = null;
-
-    //Função responsavel por popular a lista de Funcionarios na DAO.
-    public FuncionarioDAO() {
-        Statement comando = null;
-        Connection con = null;
-
-        try {
-            if (funcionarios == null) {
-                funcionarios = new ArrayList<>();
-
-                con = Database.getConnection();
-
-                // Comando SQL responsavel por obter os funcionarios no banco de dados.
-                comando = con.createStatement();
-                String sql = "select f.Id, f.Nome, f.IdCargo, c.Descricao as Cargo, f.Codigo,c.TipoCodigo"
-                        + " from Funcionario f"
-                        + " join Cargo c on c.id = f.IdCargo";
-                ResultSet result = comando.executeQuery(sql);
-
-                // Enquanto houver dados no result.
-                while (result.next()) {
-                    // Ler o dado.
-                    int id = result.getInt("id");
-                    String nome = result.getString("nome");
-                    int idCargo = result.getInt("idCargo");
-                    String cargo = result.getString("Cargo");
-                    int codigo = result.getInt("codigo");
-                    String tipoCodigo = result.getString("tipoCodigo");
-
-                    funcionarios.add(new Funcionario(id, idCargo, cargo, nome, codigo, tipoCodigo));
-                }
-            }
-        } catch (ClassNotFoundException | SQLException ex) {
-            Logger.getLogger(FuncionarioDAO.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            try {
-                comando.close();
-                con.close();
-            } catch (NullPointerException | SQLException ex) {
-                Logger.getLogger(FuncionarioDAO.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-    }
-
     //Função responsavel por inserir os funcionarios no banco de dados.
     public boolean insere(Funcionario funcionario) {
         PreparedStatement comando = null;
         Connection con = null;
 
         try {
-
+           
             con = Database.getConnection();
             String sql = "insert into Funcionario(Nome,IdCargo,Codigo) values (?,?,?)";
-            comando = con.prepareStatement(sql);            
+            comando = con.prepareStatement(sql);
             comando.setString(1, funcionario.getNome());
             comando.setInt(2, funcionario.getIdCargo());
             comando.setInt(3, funcionario.getCodigo());
             comando.executeUpdate();
-
-            funcionarios.add(funcionario);
 
             return true;
         } catch (ClassNotFoundException | SQLException ex) {
@@ -93,8 +46,53 @@ public class FuncionarioDAO {
 
         return false;
     }
-
+    
+    /***
+     * Função responsável por retornar a todos os Funcionários cadastrados no banco.
+     * @return Uma coleção de Funcionários
+     */
     public List<Funcionario> getAllFuncionarios() {
-        return funcionarios;
+        Statement comando = null;
+        Connection con = null;
+
+        try {
+            List<Funcionario> funcionarios = new ArrayList<>();
+
+            con = Database.getConnection();
+
+            // Comando SQL responsavel por obter os funcionarios no banco de dados.
+            comando = con.createStatement();
+            String sql = "select f.Id, f.Nome, f.IdCargo, c.Descricao as Cargo, f.Codigo,c.TipoCodigo"
+                    + " from Funcionario f"
+                    + " join Cargo c on c.id = f.IdCargo"
+                    + " order by f.id";
+            ResultSet result = comando.executeQuery(sql);
+
+            // Enquanto houver dados no result.
+            while (result.next()) {
+                // Ler o dado.
+                int id = result.getInt("id");
+                String nome = result.getString("nome");
+                int idCargo = result.getInt("idCargo");
+                String cargo = result.getString("Cargo");
+                int codigo = result.getInt("codigo");
+                String tipoCodigo = result.getString("tipoCodigo");
+
+                funcionarios.add(new Funcionario(id, idCargo, cargo, nome, codigo, tipoCodigo));
+            }
+
+            return funcionarios;
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(FuncionarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                comando.close();
+                con.close();
+            } catch (NullPointerException | SQLException ex) {
+                Logger.getLogger(FuncionarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        return null;
     }
 }
