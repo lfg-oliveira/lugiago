a1b35f9b2d578ff6db0c59fa05b7fe836cc8f533
Tiago <tiagoxp.desenvolvedor@gmail.com>
2023-04-12 13:50:59 -0300

    Ajuste DB

--- db-script.sql
+++ db-script.sql
@@ -18,7 +18,7 @@ create table Funcionario(
 Id bigint not null primary key auto_increment,
 Codigo bigint not null unique key,
 Nome varchar(80) not null,
-idCargo bigint not null,
+idCargo varchar(30) not null,
 foreign key (idCargo) references Cargo(Id)
 );
 
