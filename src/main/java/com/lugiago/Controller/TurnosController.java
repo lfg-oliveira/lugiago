cd5d975dda0f006df23754c2b331c383d278591f
Tiago <tiagoxp.desenvolvedor@gmail.com>
2023-04-12 01:54:42 -0300

    Implementados métodos de turnos nas controllers

--- src/main/java/com/lugiago/Controller/TurnosController.java
+++ src/main/java/com/lugiago/Controller/TurnosController.java
@@ -2,19 +2,32 @@ package com.lugiago.Controller;
 
 import com.lugiago.Model.Turno;
 import com.lugiago.Model.TurnoDAO;
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class TurnosController{
 
     private static TurnoDAO dao = new TurnoDAO();;
     
-    public static void grava (int id, int idFuncionario, String NomeFuncionario, String Cargo, Date DataInicial, Date DataFinal) {
-        Turno t = new Turno(id, idFuncionario, NomeFuncionario, Cargo, DataInicial, DataFinal);
+    public static void grava (int idFuncionario, LocalDateTime DataInicial) {
+        Turno t = new Turno(0, idFuncionario, null, null, DataInicial, null);
+        
         dao.insere(t);
     }
     
-    public static List<Turno> getFuncionarios(){
+    public static List<Turno> getTurnos(){
         return dao.getAllTurnos();
+    }
+    
+    public static boolean validaDisponibiliade(int idFuncionario, LocalDateTime DataInicial, int turnoId){
+        Turno t = new Turno(turnoId, idFuncionario, null, null, DataInicial, null);
+        
+        return dao.valida(t);
+    }
+    
+    public static boolean validaDisponibiliade(int idFuncionario, LocalDateTime DataInicial ){
+        Turno t = new Turno(0, idFuncionario, null, null, DataInicial, null);
+        
+        return dao.valida(t);
     }
 }
